5c86cae650885855ec6de6f3898d2e7574995daf
Sanjar Akhmedov <sanjakhmedov@gmail.com>
2022-12-08 23:54:03 +0500

    loops-finalTask is completed

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public static void main(String[] args) {
+        printTable(5);
+    }
+    public static void printTable(int numberTableToPrint){
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " x " + numberTableToPrint + " = " + numberTableToPrint * i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j < cathetusLength + i; j++) {
+                if (i > cathetusLength - j && j < cathetusLength ) {
+                    System.out.print(cathetusLength - j + 1);
+                } else if ( j > cathetusLength - 1 && cathetusLength > j - cathetusLength) {
+                    System.out.print(j - cathetusLength + 1);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
